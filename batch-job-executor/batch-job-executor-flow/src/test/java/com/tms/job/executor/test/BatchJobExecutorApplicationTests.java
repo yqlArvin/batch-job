c2db1b96688c53e00df3aaafb1eab7eec36b1465
yqlArvin <yql825018582@163.com>
2021-01-19 14:44:54 +0800

    任务分布式调度框架

---
+++ batch-job-executor/batch-job-executor-flow/src/test/java/com/tms/job/executor/test/BatchJobExecutorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tms.job.executor.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BatchJobExecutorApplicationTests {
+
+    @Test
+    public void test() {
+        System.out.println(11);
+    }
+
+}
