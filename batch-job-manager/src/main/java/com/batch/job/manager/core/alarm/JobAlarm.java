c67c1e4f0b4c7e005e602de6c8a1006acb4d1bb0
yqlArvin <yql825018582@163.com>
2021-02-02 21:15:33 +0800

    框架优化

---
+++ batch-job-manager/src/main/java/com/batch/job/manager/core/alarm/JobAlarm.java
@@ -0,0 +1,20 @@
+package com.batch.job.manager.core.alarm;
+
+import com.batch.job.manager.core.model.XxlJobInfo;
+import com.batch.job.manager.core.model.XxlJobLog;
+
+/**
+ * @author xuxueli 2020-01-19
+ */
+public interface JobAlarm {
+
+    /**
+     * job alarm
+     *
+     * @param info
+     * @param jobLog
+     * @return
+     */
+    public boolean doAlarm(XxlJobInfo info, XxlJobLog jobLog);
+
+}
